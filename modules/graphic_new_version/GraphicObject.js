2ff03dde6a173ddcd78337894a9f53f166514a46
bonnet_b <bonnet_b@epitech.eu>
2012-10-18 11:07:02 +0200

    new MatrixClass added, class GraphicObject added

---
+++ modules/graphic_new_version/GraphicObject.js
@@ -0,0 +1,24 @@
+/**
+ @module GraphicObject
+ @namespace Graphic
+ */
+
+var TW = TW || {};
+TW.Graphic = TW.Graphic || {};
+
+/**
+ * @type {GraphicObject}
+ */
+
+TW.Graphic.GraphicObject = function() {
+
+	/**
+	 *
+	 * @constructor
+	 */
+	function GraphicObject() {
+
+	}
+
+	return GraphicObject;
+}();
