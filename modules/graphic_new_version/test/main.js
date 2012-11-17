93e93abdb89bbb4d389e22dc6eef35f94151bf73
bonnet_b <bonnet_b@epitech.eu>
2012-11-17 22:41:19 +0100

    adding test folder to the repository for debug purpose...

---
+++ modules/graphic_new_version/test/main.js
@@ -0,0 +1,29 @@
+function main()
+{
+	var myCanvas = document.getElementById("my_Canvas");
+	var myCanvasContext = myCanvas.getContext("2d");
+
+	var myWindow = new TW.Graphic.Window(myCanvas);
+	var myLayer = new TW.Graphic.Layer({width:800, height:800});
+	var mySprite = new TW.Graphic.Sprite({x : 0, y : 0, width : 50, height : 50});
+
+	var image = new Image();
+	image.src = "test.jpg";
+	mySprite.setImage(image);
+
+	myLayer.addChild(mySprite);
+	myWindow.addChild(myLayer);
+	
+	mySprite.scale(5,5);
+	//mySprite.translate(200, 100);
+	mySprite.rotate(45);
+	mySprite.translate(200, 100);
+		
+	myWindow.draw();
+
+	//mySprite.draw(myCanvasContext);
+}
+
+window.onload = function() {
+main();
+};
