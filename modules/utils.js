898f1fc053b5bb671b3bf0151bf91252e8f0bc93
Kévin Bernard-Allies <kevin.bernard.allies@tumbleweed-studio.net>
2012-11-20 23:23:37 +0800

    a little more doc in main module pages

--- modules/utils.js
+++ modules/utils.js
@@ -2,6 +2,8 @@
  * This module contain some useful functions and helpers used in the Tumbleweed framework.
  * It include some pollyfills and a way to use inheritance.
  *
+ * Polyfills are not requested, but give a more large browser compatibility.
+ *
  * @module Utils
  * @main
  */
