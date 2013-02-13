e435fcf66e5a954c5c56c1788c79ebcb17c7af22
Jeremy Grenier <jeremy.grenier@epitech.eu>
2013-02-13 18:11:20 +0900

    Update event sample

--- samples/event/event.js
+++ samples/event/event.js
@@ -1,75 +1,32 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Olousouzian
- * Date: 6/11/12
- * Time: 10:06 AM
- * To change this template use File | Settings | File Templates.
- */
-
 window.onload = function() {
 
 	var mouseEvents = new TW.Event.MouseInput();
 
-	var keyboardEvents = new TW.Event.KeyboardInput();
+	var window = new TW.Graphic.Window(document.getElementById("eventCanvas"));
+	var rect = new TW.Graphic.Rect({zIndex: 1});
 
-	var inputMapper = new TW.Event.InputMapper();
+	rect.setFillColor('#00FF00');
 
-	var eventCombination = new TW.Event.EventCombination(keyboardEvents);
+	rect.x = 200;
+	rect.y = 50;
+	rect.width = 10;
+	rect.height = 10;
+
+	window.addChild(rect);
+	window.draw();
 
 	mouseEvents.contextMenuActive = false;
 
-	eventCombination.addCombination("ATTACK_COMBO", ["KEY_A", "KEY_Z", "KEY_E"]);
-	eventCombination.addCombination("ATTACK_COMBO2", ["KEY_S", "KEY_Z"]);
-
-	eventCombination.addListener("ATTACK_COMBO", [{event:"KEY_A", value:true}, {event:"KEY_Z", value:true}, {event:"KEY_E", value:true}], function(event, new_value, object) {
-		console.log("ATTACK_COMBO");
-		console.log("");
-	});
-
-	eventCombination.addListener("ATTACK_COMBO2", [{event:"KEY_S", value:true}, {event:"KEY_Z", value:true}], function(event, new_value, object) {
-		console.log("ATTACK_COMBO2");
-		console.log("");
-	});
-/*
 	mouseEvents.addListener("MOUSE_MOVE", function(event, new_value, object) {
+		rect.setPosition(new_value.x, new_value.y);
+		rect.draw();
 		console.log(new_value);
 	});
 
-	mouseEvents.addListener("MOUSE_MOVE", {x: 500, y: 500}, function(event, new_value, object) {
-		console.log("Ca marche !");
-	});
-*/
-	/*
-	inputMapper.addEvent("ATTACK");
+	var list = document.getElementById('list');
 
-	inputMapper.addEvent("MOVE_UP");
-	inputMapper.addEvent("MOVE_DOWN");
-	inputMapper.addEvent("MOVE_LEFT");
-	inputMapper.addEvent("MOVE_RIGHT");
-
-	inputMapper.bindEvent("MOVE_UP", "KEY_Z", keyboardEvents);
-
-	mouseEvents.addListener("MOUSE_BUTTON_RIGHT", TW.Event.MouseInput.BUTTON_PRESSED, function(event, new_value, object) {
-		alert("Ca marche !");
-	});
-
-	keyboardEvents.addListener("KEY_B", function(event, new_value, object) {
-		inputMapper.bindListen("ATTACK", mouseEvents);
-	});
-	keyboardEvents.addListener("KEY_C", function(event, new_value, object) {
-		inputMapper.stopBindListen();
-	});
-
-	inputMapper.addListener(
-		function(event, new_value, object) {
-			console.log(event);
-			console.log(new_value);
-			console.log("");
-		});
-
-	console.log(inputMapper.getRealEvent("ATTACK"));
-	console.log(inputMapper.getNoMappedEvents());
-	*/
-
+	list.rows[0].onclick = function() {
+		alert(this.rowIndex);
+	};
 
 };
