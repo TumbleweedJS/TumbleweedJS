d53c9e34bc69128fc2627df6ac8191418cb5e7c9
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2013-03-12 11:56:44 +0800

    implemented Utils.copyParam

--- modules/utils/Inheritance.js
+++ modules/utils/Inheritance.js
@@ -58,5 +58,50 @@ var TW = TW || {};
         return new_instance;
     };
 
+	/**
+	 * copy all allowed variables from `params` to `target`, using `defaultContext` for set default values.
+	 *
+	 * `copyParam` is used principally for easily copy parameters from hash objects.
+	 * All variables must be present in `defaultContext`,
+	 * so adding an unespected variable will not override `target` object.
+	 * All values in `defayultContext` are also copied as default values.
+	 *
+	 * If you want to allow some properties, but not to set default value,
+	 * you can create the property and set it to undefined.
+	 *
+	 * @examples:
+	 *
+	 *      var target = {};
+	 *      var default_context = {
+	 *          foo:    "default value",
+	 *          bar:    33,
+	 *          baz:    undefined           // baz is allowed, but has not default value.
+	 *      };
+	 *
+	 *      Utils.copyParam(target, { foo: "some value", unknown: 3 }, default_context);
+	 *      console.log(target);
+	 *      // Object {foo: "some value", bar: 33}
+	 *      //unknown is not copied because not allowed.
+	 *
+	 * @method copyParam
+	 *
+	 *
+	 * @param {Object} target
+	 * @param {Object} params
+	 * @param {Object} default_context
+	 */
+	TW.Utils.copyParam = function(target, params, default_context) {
+		for (var i in default_context) {
+			if (default_context.hasOwnProperty(i)) {
+				if (params.hasOwnProperty(i)) {
+					target[i] = params[i];
+				} else if (default_context[i] !== undefined) {
+					target[i] = default_context[i];
+				}
+			}
+		}
+	};
+
+
 }(TW));
 
---
+++ test/utils/Inherit.js
@@ -0,0 +1,20 @@
+
+//Qunit Test for View class
+
+module("Inheritance");
+
+test("copyParam helper", function() {
+	var target = {};
+	var default_context = {
+		foo:    "default value",
+		bar:    33,
+		baz:    undefined           // baz is allowed, but has not default value.
+	};
+
+	TW.Utils.copyParam(target, { foo: "some value", unknown: 3 }, default_context);
+
+	deepEqual(target,  {
+		foo:    "some value",
+		bar:    33
+	});
+});
