76f676d3a281d3e4bdd25fa020ed6e5cd4eaf289
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2013-08-14 17:46:33 +0200

    code the GameObject class, with tests

--- modules/Core/GameObject.js
+++ modules/Core/GameObject.js
@@ -5,37 +5,167 @@
 
 var TW = TW || {};
 
-define([], function() {
+define(['TW/Utils/inherit', 'TW/Utils/mixin', 'TW/Event/EventProvider'],
+       function(inherit, mixin, EventProvider) {
 
 	TW.Core = TW.Core || {};
 
 	/**
-	 * TODO description GameObject
+	 * Base class for any game object wich want to use components.
 	 *
-	 * --Le concept GameObject
-	 * -- La classe GameObject -- les mixins
-	 * -- Les Components possible, et comment les développer.
+	 * GameObject is a special class destined to contains many components,
+	 * wich can have other components dependency.
+	 * It's a way to use inheritance more flexible than classical `inherit` method,
+	 * and more strict than `mixin` method.
+	 *
+	 * A GameObject can have zero or more components, listed has dependencies.
+	 * Each components can have it's own dependendies.
+	 * All components needed are merged in a new class inherited from GameObject.
+	 * The new created class can be used like any other class.
+	 *
+	 *
+	 * A Component is a special class crated by `GameObject.makeComponent`.
+	 *
+	 *
+	 *     var Comp1 = GameObject.makeComponent('comp1', [], function(params) {
+	 *       //component constructor
+	 *     });
+	 *
+	 *     var Class1 = GameObject.makeClass([Comp1], function(params) {
+	 *       //class constructor
+	 *       //called after all components constructor
+	 *     });
+	 *
+	 *     //params are passed to all composant constructors.
+	 *     var instance = new Class1(params);
+	 *
+	 *
+	 *     //Component can also be called alone.
+	 *     var instance2 = new Comp1(params);
+	 *
+	 *
+	 *     //If you want to modify a GameObject class, you should use a new level of inheritance
+	 *     function MyClass() {
+	 *       Class1.call(this, params);
+	 *     };
+	 *
+	 *     inherit(MyClass, Class1);
+	 *
+	 *
+	 *     MyClass.prototype.func = function() {};
+	 *
+	 *
+	 * **This constructor should never be called directly.
+	 * Use `GameObject.makeClass` instead.**
 	 *
 	 * @class GameObject
 	 * @constructor
 	 */
-	function GameObject() {
+	function GameObject(deps, params) {
+		this._components = this._components || [];
 
-		this._components = [];
-
+		deps.forEach(function(component) {
+			if (this._components.indexOf(component.componentName) === -1) {
+				component.call(this, params);
+			}
+		}, this);
 	}
 
 	/**
+	 * Create a new class extending GameObject,
+	 * containing all components listed in `deps` merged directly
+	 * in the class.
+	 *
+	 * This method should be used to make a GameObject inherited class,
+	 * instead of classical inheritance.
+	 *
+	 * @example
+	 *
+	 *    var myClass = GameObject.makeClass([Rect, Collisionable], function(params) {
+	 *        //constructor here
+	 *    });
+	 *
+	 *    var instance = new myClass(params);
+	 *
+	 * @method makeClass
+	 * @param {Component[]} deps list of all components to mix in the class.
+	 * @param {Function} [ctor] constructor of the new created class.
+	 *  @param {Object} [ctor.params] parameters passed to constructor when a new instance is created.
+	 *    Should be a litteral object.
+	 * @return {Function} new class wich can be extended.
+	 */
+	GameObject.makeClass = function(deps, ctor) {
+		var obj = function(params) {
+			GameObject.call(this, deps, params);
+			if (ctor !== undefined) {
+				ctor.apply(this, arguments);
+			}
+		};
+		inherit(obj, GameObject);
+
+		deps.forEach(function(component) {
+			mixin(obj.prototype, component.prototype);
+		});
+
+		return obj;
+	};
+
+
+	/**
+	 * Create a new component class wich can be used as a dependency for
+	 * GameObject classes and others components.
+	 *
+	 * All component listed in `deps` are mixed directly in the created component class.
+	 *
+	 * This method should be used to create new components.
+	 *
+	 * Note: a component is also a gameObject and can be instancied like any other class.
+	 *
+	 * @method makeComponent
+	 * @param {String} name component name
+	 * @param {Component[]} deps list of all components to mix in the class.
+	 * @param {Function} [ctor] constructor of component.
+	 *  @param {Object} [ctor.params] parameters passed to constructor
+	 *    when a new instance of GameObject containing this component is created.
+	 *    Should be a litteral object.
+	 * @returns {Component} the new created component class.
+	 */
+	GameObject.makeComponent = function(name, deps, ctor) {
+		var obj = function() {
+			GameObject.call(this, deps);
+			if (ctor !== undefined) {
+				ctor.apply(this, arguments);
+			}
+			this._components.push(name);
+		};
+		inherit(obj, GameObject);
+		obj.componentName = name;
+
+		deps.forEach(function(component) {
+			mixin(obj.prototype, component.prototype);
+		});
+
+
+		return obj;
+	};
+
+	inherit(GameObject, EventProvider);
+
+	/**
+	 * List all components wich are included in the class.
+	 *
 	 * @method listComponents
-	 * @returns {Array} components
+	 * @returns {String[]} components
 	 */
 	GameObject.prototype.listComponents = function() {
 		return this._components;
 	};
 
 	/**
+	 * Check if a component has been included in the class.
+	 *
 	 * @method hasComponent
-	 * @param name searched behavior name
+	 * @param {String} name searched behavior name
 	 * @returns {Boolean}
 	 */
 	GameObject.prototype.hasComponent = function(name) {
@@ -48,6 +178,8 @@ define([], function() {
 		return false;
 	};
 
+
 	TW.Core.GameObject = GameObject;
 	return GameObject;
 });
+
--- test/Core/GameObject.js
+++ test/Core/GameObject.js
@@ -4,109 +4,95 @@ define(['TW/Core/GameObject', 'TW/Utils/inherit'], function(GO, inherit) {
 
 
 	test('empty GO', function() {
-		var obj = new GO();
+		var obj = new (GO.makeClass([]))();
 
 		deepEqual(obj.listComponents(), []);
 		strictEqual(obj.hasComponent('Acomp'), false);
 	});
 
 	test("GO methods after added two component", function() {
-		var FirstComponent = function() {};
-		FirstComponent.name = 'first';
-		var SecondComponent = function() {};
-		SecondComponent.name = 'second';
-		var DerivatedGO = function() {};
+		var FirstComponent = GO.makeComponent('first', []);
+		var SecondComponent = GO.makeComponent('second', [], function() {});
 
-		inherit(DerivatedGO, GO);
-		GO.implement(DerivatedGO, FirstComponent, SecondComponent);
+		var DerivatedGO = GO.makeClass([FirstComponent, SecondComponent]);
 
 		var instance = new DerivatedGO();
-		deepEqual(instance.listComponents(), ['firstComponent', 'secondComponent'], "check listComponents()");
+		deepEqual(instance.listComponents(), ['first', 'second'], "check listComponents()");
 		equal(instance.hasComponent('first'), true, "has first Component");
 		equal(instance.hasComponent('second'), true, "has second Component");
 		equal(instance.hasComponent('third'), false, "has not third Component");
 	});
 
 	test("access properties using two small component", function() {
-		var FirstComponent = function() {
+		var FirstComponent = GO.makeComponent('first', [], function() {
 			this.first = true;
-		};
-		FirstComponent.name = 'first';
+		});
 
-		FirstComponent.firstMethod = function() {
+		FirstComponent.prototype.firstMethod = function() {
 			this.first = false;
 		};
 
-		var SecondComponent = function() {
+		var SecondComponent = GO.makeComponent('second', [], function() {
 			this.second = 33;
-		};
-		SecondComponent.deps = ['first'];
+		});
 
-		var DerivatedGO = function() {
+		var DerivatedGO = GO.makeClass([FirstComponent, SecondComponent], function() {
 			this.derivated = "ok";
-		};
-		DerivatedGO.method = function() {
+		});
+
+		DerivatedGO.prototype.method = function() {
 			this.derivated = "perfect";
 		};
-
-		inherit(DerivatedGO, GO);
-		GO.implement(DerivatedGO, FirstComponent, SecondComponent);
-
 
 
 		var instance = new DerivatedGO();
 
 		strictEqual(instance.derivated, "ok", "class properties are always present.");
-		strictEqual(instance.first, true, "first component propertie is valid.");
-		strictEqual(instance.second, 33, "second component propertie is valid.");
+		strictEqual(instance.first, true, "first component property is valid.");
+		strictEqual(instance.second, 33, "second component property is valid.");
 
-		strictEqual(instance.firstMethod, FirstComponent.firstMethod, "method are also copied.");
+		strictEqual(instance.firstMethod, FirstComponent.prototype.firstMethod, "method are also copied.");
 
 		instance.method();
 		instance.firstMethod();
 
 		strictEqual(instance.derivated, "perfect", "class methods works well.");
 		strictEqual(instance.first, false, "component methods also works well.");
-
 		strictEqual(instance.name, undefined, "name components should not be copied.");
-		strictEqual(instance.deps, undefined, "dependencies should not be copied.");
 	});
 
 	test('using components with dependencies', function() {
-		var FirstComponent = function() {};
-		FirstComponent.deps = ['./TestComponent'];
-		FirstComponent.method = function() {
-			strictEqual(this.attribute, "test", 'Remote required component is loaded');
+		var FirstComponent = GO.makeComponent('first', []);
+		FirstComponent.prototype.method = function() {
+			return "2";
 		};
 
-		var DerivatedGO = function() {};
+		var SecondComponent = GO.makeComponent('second', [FirstComponent]);
 
-		inherit(DerivatedGO, GO);
-		GO.implement(DerivatedGO, FirstComponent);
+		var DerivatedGO = GO.makeClass([SecondComponent]);
 
 		var instance = new DerivatedGO();
 
 		expect(1);
-		instance.method();
+		strictEqual(instance.method(), "2", 'required component is loaded');
 	});
 
 	test('passing params to constructors', function() {
-		var FirstComponent = function(params) {
-			this.result = params.var1 * params.var2;
-		};
+		var FirstComponent = GO.makeComponent('first', [], function(params) {
+			this.mult = params.var1 * params.var2;
+		});
 
-		var DerivatedGO = function(params) {
-			GO(params);
-		};
-
-		inherit(DerivatedGO, GO);
-		GO.implement(DerivatedGO, FirstComponent);
+		var DerivatedGO = GO.makeClass([FirstComponent], function(params) {
+			this.add = params.var1 + params.var2;
+		});
 
 		var instance = new DerivatedGO( { var1: 3, var2: 3 });
-		strictEqual(instance.result, 9);
+		strictEqual(instance.mult, 9, 'params are passed to component constructor');
+		strictEqual(instance.add, 6, 'params are passed to mult constructor');
 
 		instance = new DerivatedGO( { var1: 4, var2: 2 });
-		strictEqual(instance.result, 8);
+		strictEqual(instance.mult, 8, 'params are passed to component constructor');
+		strictEqual(instance.add, 6, 'params are passed to class constructor');
 
 	});
 });
